c30b604afc46b5a6ff97918139e4849a82cdcf97
Joe Alcorn <joealcorn123@gmail.com>
2014-09-07 14:46:01 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='python-xbox',
+    version='0.0.1',
+    url='https://github.com/buttscicles/xbox/',
+    author='Joe Alcorn',
+    author_email='joealcorn123@gmail.com',
+    description=(
+        "A wrapper around Microsoft's undocumented Xbox one APIs"
+    ),
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
+
