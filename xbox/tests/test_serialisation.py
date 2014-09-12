e3d11aa23e25936b4d6c037317bb3e8ed54bb32e
Joe Alcorn <joealcorn123@gmail.com>
2014-09-12 01:26:25 +0100

    use id instead of hash

--- xbox/tests/test_serialisation.py
+++ xbox/tests/test_serialisation.py
@@ -4,6 +4,6 @@ import pickle
 def test_pickle_clip(clip):
     pickled = pickle.dumps(clip)
     restored = pickle.loads(pickled)
-    assert hash(clip) != hash(restored)
+    assert id(clip) != id(restored)
     for key, value in clip.__dict__.iteritems():
         assert restored.__dict__[key] == value
